96409f364d2242e6a199f7a2a8ab30e2df30963b
Cao Nam <caonam.uit@gmail.com>
2024-08-10 17:06:19 +0700

    handle onClick login, register for client

--- client/src/containers/Public/Header.js
+++ client/src/containers/Public/Header.js
@@ -2,25 +2,29 @@ import React, { useCallback } from "react";
 import logo from "../../assets/logowithoutbg.png";
 import { Button } from "../../components";
 import icons from "../../ultils/icons";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { path } from "../../ultils/constant";
 
 const { CiCirclePlus } = icons;
 
 const Header = () => {
   const navigate = useNavigate();
-  const goLogin = useCallback(() => {
-    navigate(path.LOGIN);
+  // Flag là login hoặc register
+  const goLogin = useCallback((flag) => {
+    navigate(path.LOGIN, { state: { flag } });
   }, []);
 
   return (
     <div className="w-1100">
       <div className="w-full flex items-center justify-between">
-        <img src={logo} class="w-[240px] h-[70px] object-contain" alt="logo" />
+        <Link to={"/"}>
+          <img src={logo} class="w-[240px] h-[70px] object-contain" alt="logo" />
+        </Link>
+
         <div className="flex items-center gap-1">
           <small>Phongtro123.com xin chào !</small>
-          <Button text={"Đăng nhập"} textColor="text-white" bgColor="bg-[#3961fb]" onClick={goLogin} />
-          <Button text={"Đăng ký"} textColor="text-white" bgColor="bg-[#3961fb]" onClick={goLogin} />
+          <Button text={"Đăng nhập"} textColor="text-white" bgColor="bg-[#3961fb]" onClick={() => goLogin(false)} />
+          <Button text={"Đăng ký"} textColor="text-white" bgColor="bg-[#3961fb]" onClick={() => goLogin(true)} />
           <Button text={"Đăng tin miễn phí"} textColor="text-white" bgColor="bg-secondary2" IcAfter={CiCirclePlus} />
         </div>
       </div>
--- client/src/containers/Public/Login.js
+++ client/src/containers/Public/Login.js
@@ -1,22 +1,50 @@
-import React from 'react'
-import { InputForm, Button } from '../../components'
+import React, { useState, useEffect } from "react";
+import { InputForm, Button } from "../../components";
+import { useLocation } from "react-router-dom";
 
 const Login = () => {
+  const location = useLocation();
+  console.log("lo: ", location);
+  const [isRegister, setIsRegister] = useState(location.state?.flag);
+
+  useEffect(() => {
+    setIsRegister(location.state?.flag);
+  }, [location.state?.flag]);
+
   return (
-    <div className='bg-white mt-3 flex flex-col w-[600px] p-[30px] pb-[100px] rounded-md shadow-sm'>
-        <h3 className='font-semibold text-2xl mb-3'>Đăng nhập</h3>
-        <div className="w-full flex flex-col gap-5">
-            <InputForm label={'Số điện thoại'}/>
-            <InputForm label={'Mật khẩu'}/>
-            <Button text='Đăng nhập' bgColor='bg-secondary1' textColor='text-white' fullWidth/>
-        </div>
+    <div className="bg-white mt-3 flex flex-col w-[600px] p-[30px] pb-[100px] rounded-md shadow-sm">
+      <h3 className="font-semibold text-2xl mb-3">{isRegister ? "Đăng ký tài khoản" : "Đăng nhập"}</h3>
+      <div className="w-full flex flex-col gap-5">
+        {isRegister && <InputForm label={"Họ tên"} />}
+        <InputForm label={"Số điện thoại"} />
+        <InputForm label={"Mật khẩu"} />
+        <Button text={isRegister ? "Đăng ký" : "Đăng nhập"} bgColor="bg-secondary1" textColor="text-white" fullWidth />
+      </div>
 
-        <div className="mt-7 flex items-center justify-between">
+      <div className="mt-7 flex items-center justify-between">
+        {isRegister ? (
+          <small>
+            Bạn đã có tài khoản?{" "}
+            <span
+              onClick={() => {
+                setIsRegister(false);
+              }}
+              className="text-blue-500 hover:underline cursor-pointer"
+            >
+              Đăng nhập ngay
+            </span>
+          </small>
+        ) : (
+          <>
             <small className="text-[blue] hover:text-[orange] cursor-pointer">Bạn quên mật khẩu?</small>
-            <small className="text-[blue] hover:text-[orange] cursor-pointer">Tạo tài khoản mới</small>
-        </div>
+            <small onClick={() => setIsRegister(true)} className="text-[blue] hover:text-[orange] cursor-pointer">
+              Tạo tài khoản mới
+            </small>
+          </>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
