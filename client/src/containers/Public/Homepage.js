b46c38f12fa3afb283528082d656f47f839c3592
Cao Nam <caonam.uit@gmail.com>
2024-08-21 22:26:14 +0700

    design sidebar p1

--- client/src/containers/Public/Homepage.js
+++ client/src/containers/Public/Homepage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { text } from "../../ultils/constant";
-import { Province } from "../../components";
+import { Province, ItemSidebar } from "../../components";
 import { List, Pagination } from "./index";
 // useParams để lấy /2, useSearchParams lấy /?page=2
 import { useParams, useSearchParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Homepage = () => {
   const [params] = useSearchParams();
+  const { categories } = useSelector((state) => state.app);
   console.log(params.get("page"));
 
   return (
@@ -23,7 +25,11 @@ const Homepage = () => {
           <div className="h-[500px]"></div>
         </div>
 
-        <div className="w-[30%] border-green-500 border">Sidebar</div>
+        <div className="w-[30%] border-green-500 flex flex-col gap-4 justify-start items-center">
+          <ItemSidebar content={categories} title="Danh sách cho thuê" />
+          <ItemSidebar title="Xem theo giá" />
+          <ItemSidebar title="Xem theo diện tích" />
+        </div>
       </div>
     </div>
   );
